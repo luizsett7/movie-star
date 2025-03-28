39ffacc0834470d8749fc1d14a29d558188d189c
Luiz Guilherme <luizsett7@gmail.com>
2025-03-27 21:34:17 -0300

    templates created

---
+++ templates/footer.php
@@ -0,0 +1,35 @@
+<footer id="footer">
+    <div id="social-container">
+        <ul>
+            <li>
+                <a href="#"><i class="fab fa-facebook-square"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </li>
+            <li>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+        <ul>
+            <li>
+                <a href="#">Adicionar filme</a>
+            </li>
+            <li>
+                <a href="#">Adicionar crítica</a>
+            </li>
+            <li>
+                <a href="#">Entrar / Registrar</a>
+            </li>
+        </ul>
+    </div>
+    <p>&copy; 2025 MovieStar Company</p>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.js" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.js" />
+</body>
+
+</html>
