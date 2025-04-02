961f6dd88b12d2de285a09acad20df4310e73174
Luiz Guilherme <luizsett7@gmail.com>
2025-04-02 16:56:07 -0300

    login and register added

--- templates/header.php
+++ templates/header.php
@@ -1,8 +1,20 @@
 <?php
 require_once("globals.php");
 require_once("db.php");
+require_once("models/Message.php");
+require_once("dao/UserDAO.php");
 
-$flassMessage = [];
+$message = new Message($BASE_URL);
+
+$flassMessage = $message->getMessage();
+
+if (!empty($flassMessage["msg"])) {
+    $message->clearMessage();
+}
+
+$userDao = new UserDAO($conn, $BASE_URL);
+
+$userData = $userDao->verifyToken(false);
 
 ?>
 <!DOCTYPE html>
@@ -36,16 +48,30 @@ $flassMessage = [];
             </form>
             <div class="collapse navbar-collapse" id="navbar">
                 <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
-                    </li>
+                    <?php if ($userData): ?>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>newmovie.php" class="nav-link"><i class="far fa-plus-square"></i> Incluir Filmes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>dashboard.php" class="nav-link">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>editprofile.php" class="nav-link bold"><?= $userData->name; ?></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>logout.php" class="nav-link">Sair</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <a href="<?= $BASE_URL ?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
     </header>
     <?php if (!empty($flassMessage["msg"])): ?>
-
+        <div class="msg-container">
+            <p class="msg <?= $flassMessage["type"] ?>"><?= $flassMessage["msg"] ?></p>
+        </div>
     <?php endif; ?>
-    <div class="msg-container">
-        <p class="msg <?= $flassMessage["type"] ?>"><?= $flassMessage["msg"] ?></p>
-    </div>
