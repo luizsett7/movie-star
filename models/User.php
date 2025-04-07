b2373362e34f7022307e884d228fbb7d79b0c35b
Luiz Guilherme <luizsett7@gmail.com>
2025-04-07 17:29:30 -0300

    image and password edition changed

--- models/User.php
+++ models/User.php
@@ -18,12 +18,17 @@ class User
 
     public function generateToken()
     {
-        return bin2hex(random_bytes((50)));
+        return bin2hex(random_bytes(50));
     }
 
     public function generatePassword($password)
     {
         return password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function imageGenerateName()
+    {
+        return bin2hex(random_bytes(60)) . ".jpg";
     }
 }
 
