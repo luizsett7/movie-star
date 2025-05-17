e355fd01ca1394c6633d07c5ab63367b941782ba
Luiz Guilherme <luizsett7@gmail.com>
2025-05-17 16:25:05 -0300

    update movie added

--- movie_process.php
+++ movie_process.php
@@ -72,6 +72,59 @@ if ($type === "create") {
     } else {
         $message->setMessage("Informações inválidas.", "error", "index");
     }
+} else if ($type === "update") {
+    $title = filter_input(INPUT_POST, "title");
+    $description = filter_input(INPUT_POST, "description");
+    $trailer = filter_input(INPUT_POST, "trailer");
+    $category = filter_input(INPUT_POST, "category");
+    $length = filter_input(INPUT_POST, "length");
+    $id = filter_input(INPUT_POST, "id");
+
+    $movieData = $movieDao->findById($id);
+
+    if ($movieData) {
+        if ($movieData->users_id == $userData->id) {
+
+            if (!empty($title) && !empty($description) && !empty($category)) {
+                $movieData->title = $title;
+                $movieData->description = $description;
+                $movieData->trailer = $trailer;
+                $movieData->category = $category;
+                $movieData->length = $length;
+
+                if (isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])) {
+
+                    $image = $_FILES["image"];
+                    $imageTypes = ["image/jpeg", "image/jpg", "image/png"];
+                    $jpgArray = ["image/jpeg", "image/jpg"];
+
+                    if (in_array($image["type"], $imageTypes)) {
+                        if (in_array($image["type"], $jpgArray)) {
+                            $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+                        } else {
+                            $imageFile = imagecreatefrompng($image["tmp_name"]);
+                        }
+
+                        $movie = new Movie();
+
+                        $imageName = $movie->imageGenerateName();
+
+                        imagejpeg($imageFile, "./img/movies/" . $imageName, 100);
+
+                        $movie->image = $imageName;
+                    }
+                }
+
+                $movieDao->update($movieData);
+            } else {
+                $message->setMessage("Você precisa adicionar pelo menos: título, descrição e categoria!", "error", "back");
+            }
+        } else {
+            $message->setMessage("Informações inválidas.", "error", "index");
+        }
+    } else {
+        $message->setMessage("Informações inválidas.", "error", "index");
+    }
 } else {
     $message->setMessage("Informações inválidas.", "error", "index");
 }
