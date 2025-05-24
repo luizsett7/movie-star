d8610f4a9cd3ca95ac721b0d42fbfcba6d9524f7
Luiz Guilherme <luizsett7@gmail.com>
2025-05-24 16:23:43 -0300

    hamburguer menu fixed

--- templates/footer.php
+++ templates/footer.php
@@ -27,9 +27,9 @@
     </div>
     <p>&copy; 2025 MovieStar Company</p>
 </footer>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.js" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.js" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.js"></script>
 </body>
 
 </html>
